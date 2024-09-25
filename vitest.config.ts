75d223fc590f1c08baf6492c631e8bca42111eac
LexRiver <lexriver@gmail.com>
2024-09-25 19:33:14 +0300

    config fix

--- vitest.config.ts
+++ vitest.config.ts
@@ -3,7 +3,8 @@ import { defineConfig } from 'vitest/config'
 export default defineConfig({
     test:{
         exclude: [
-            './out/**'
+            './out/**',
+            './node_modules/**'
         ]
     }
 })
